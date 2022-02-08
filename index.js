4a3df5dc1207b0cae7d9769e23b8e2ae1e6e88be
Denis <denis@releaseband.com>
2022-02-08 11:42:46 +0300

    fix: change pullrequestTitle

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
       {
         assets: ['CHANGELOG.md'],
         baseRef: 'main',
+        pullrequestTitle: `chore(release): update release<%= nextRelease.version ? \` \${nextRelease.version}\` : "" %> [skip ci]`,
       },
     ],
   ],
